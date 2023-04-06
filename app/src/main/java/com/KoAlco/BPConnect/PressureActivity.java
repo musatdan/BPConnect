e3b1abfe62206c2987585ac96597ff54e95048e1
musatdan <musatdan@yandex.ru>
2023-04-06 17:31:13 +0300

    Added LUX and Temp buttons, changes in design

--- app/src/main/java/com/KoAlco/BPConnect/PressureActivity.java
+++ app/src/main/java/com/KoAlco/BPConnect/PressureActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattServer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -23,6 +22,7 @@ import com.welie.blessed.PhyType;
 import com.welie.blessed.WriteType;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.charset.StandardCharsets;
@@ -38,13 +38,37 @@ public class PressureActivity extends AppCompatActivity {
     private boolean searching = false;
     private BluetoothCentralManager btManager;
 
-    private final UUID UUID_Service = UUID.fromString("e7810004-73ae-499d-8c15-faa9aef0c3f2");
-    private final UUID UUID_Characteristic = UUID.fromString("e7810004-73ae-499d-8c15faa9aef0c3f2");
+    private final UUID UUID_Service = UUID.fromString("e7810001-73ae-499d-8c15-faa9aef0c3f2");
+    private final UUID UUID_Characteristic = UUID.fromString("e7810004-73ae-499d-8c15-faa9aef0c3f2");
 
+
+    private boolean enabled_spo = false;
+    private boolean enabled_lux = false;
+    private boolean enabled_temp = false;
+
+    @Subscribe
+    public void onMessageEvent (MessageEvent messageEvent)
+    {
+        switch(messageEvent.message)
+        {
+            case "BLE Disconnected":
+            {
+                EventBus.getDefault().unregister(this);
+                btManager.cancelConnection(btDevice);
+
+                PressureActivity.this.finish();
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pressure);
 
         discovered = false;
         EventBus.getDefault().register(this);
@@ -72,25 +96,83 @@ public class PressureActivity extends AppCompatActivity {
             }
         }, 10000);
 
-        Button btn_start = findViewById(R.id.button_start);
-        btn_start.setOnClickListener(new View.OnClickListener() {
+        TextView textName = findViewById(R.id.textNameMeasure);
+        Button btn_spo = findViewById(R.id.button_spo);
+        Button btn_lux = findViewById(R.id.button_lux);
+        Button btn_temp = findViewById(R.id.button_temp);
+
+        btn_spo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Start();
+                if(!enabled_spo) {
+                    StartSPO();
+                    enabled_spo = true;
+                    enabled_temp = false;
+                    enabled_lux = false;
+
+                    btn_spo.setText("стоп spo2");
+                    btn_lux.setText("старт lux");
+                    btn_temp.setText("старт temp");
+                    textName.setText("SPO2");
+                }
+                else {
+                    StopSPO();
+                    enabled_spo = false;
+
+                    btn_spo.setText("старт spo2");
+                    textName.setText("");
+                }
             }
         });
 
-        Button btnStop = findViewById(R.id.button_stop);
-        btnStop.setOnClickListener(new View.OnClickListener() {
+        btn_lux.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Stop();
+                if(!enabled_lux) {
+                    StartLUX();
+                    enabled_spo = false;
+                    enabled_temp = false;
+                    enabled_lux = true;
+
+                    btn_spo.setText("старт spo2");
+                    btn_lux.setText("стоп lux");
+                    btn_temp.setText("старт temp");
+                    textName.setText("LUX");
+                }
+                else {
+                    StopLUX();
+                    enabled_lux = false;
+
+                    btn_lux.setText("старт lux");
+                    textName.setText("");
+                }
             }
         });
 
+        btn_temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!enabled_temp) {
+                    StartTEMP();
+                    enabled_spo = false;
+                    enabled_temp = true;
+                    enabled_lux = false;
 
+                    btn_spo.setText("старт spo2");
+                    btn_lux.setText("старт lux");
+                    btn_temp.setText("стоп temp");
+                    textName.setText("TEMP");
+                }
+                else {
+                    StopTEMP();
+                    enabled_temp = false;
 
-        setContentView(R.layout.activity_pressure);
+                    btn_temp.setText("старт temp");
+                    textName.setText("");
+                }
+            }
+        });
+
     }
 
     private final BluetoothPeripheralCallback peripheralCallback = new BluetoothPeripheralCallback() {
@@ -119,17 +201,28 @@ public class PressureActivity extends AppCompatActivity {
                 Toast.makeText(PressureActivity.this, "Подключено успешно", Toast.LENGTH_SHORT).show();
             }
 
-            if(characteristic.equals(UUID_Characteristic))
+            if(characteristic.getUuid().equals(UUID_Characteristic))
             {
-                TextView valueSPO = findViewById(R.id.spo2value);
+                TextView textValue = findViewById(R.id.textValue);
 
-                int valuespo2 = Byte.toUnsignedInt(value[0]);
+                int valueToText = 0;
 
-                valueSPO.setText(String.valueOf(valuespo2) + " %");
+                if(enabled_lux)
+                    valueToText = (Byte.toUnsignedInt(value[0]) << 8) + Byte.toUnsignedInt(value[1]);
+                else
+                    valueToText = Byte.toUnsignedInt(value[0]);
+
+                if(enabled_spo)
+                    textValue.setText(String.valueOf(valueToText) + " %");
+                else if (enabled_lux)
+                    textValue.setText(String.valueOf(valueToText) + " люкс");
+                else if (enabled_temp)
+                    textValue.setText(String.valueOf(valueToText) + " °C");
 
             }
 
             discovered = true;
+
         }
 
         @Override
@@ -219,7 +312,7 @@ public class PressureActivity extends AppCompatActivity {
         }, 2000);
     }
 
-    private void Start ()
+    private void StartSPO ()
     {
         String str = "SPO2enable";
         if (discovered) {
@@ -232,9 +325,61 @@ public class PressureActivity extends AppCompatActivity {
 
     }
 
-    private void Stop ()
+    private void StartLUX ()
+    {
+        String str = "LUXenable";
+        if (discovered) {
+            btDevice.writeCharacteristic(UUID_Service,
+                    UUID.fromString("e7810001-73ae-499d-8c15-faa9aef0c3f2"),
+                    str.getBytes(StandardCharsets.UTF_8),
+                    WriteType.WITHOUT_RESPONSE);
+        } else
+            Toast.makeText(this, "Подождите подключения...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void StartTEMP ()
+    {
+        String str = "TEMPenable";
+        if (discovered) {
+            btDevice.writeCharacteristic(UUID_Service,
+                    UUID.fromString("e7810001-73ae-499d-8c15-faa9aef0c3f2"),
+                    str.getBytes(StandardCharsets.UTF_8),
+                    WriteType.WITHOUT_RESPONSE);
+        } else
+            Toast.makeText(this, "Подождите подключения...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void StopSPO ()
     {
         String str = "SPO2disable";
+        if (discovered) {
+            btDevice.writeCharacteristic(UUID_Service,
+                    UUID.fromString("e7810001-73ae-499d-8c15-faa9aef0c3f2"),
+                    str.getBytes(StandardCharsets.UTF_8),
+                    WriteType.WITHOUT_RESPONSE);
+        } else
+            Toast.makeText(this, "Подождите подключения...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void StopLUX ()
+    {
+        String str = "LUXdisable";
+        if (discovered) {
+            btDevice.writeCharacteristic(UUID_Service,
+                    UUID.fromString("e7810001-73ae-499d-8c15-faa9aef0c3f2"),
+                    str.getBytes(StandardCharsets.UTF_8),
+                    WriteType.WITHOUT_RESPONSE);
+        } else
+            Toast.makeText(this, "Подождите подключения...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void StopTEMP ()
+    {
+        String str = "TEMPdisable";
         if (discovered) {
             btDevice.writeCharacteristic(UUID_Service,
                     UUID.fromString("e7810001-73ae-499d-8c15-faa9aef0c3f2"),
